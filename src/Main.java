69c69b04ecdffbf72d839f97c583adde3f31a0ee
sachko65 <levchencow@gmail.com>
2023-09-10 13:10:12 +0300

    test 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Над роботою старався Левченков Ігор групи ПЗ-20-1/9");
+        System.out.println(LocalDate.now());
     }
 }
